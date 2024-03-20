7d38064cd40836f3bdd145df4c5d4ee0d662c75e
emreyldzgl <emreyildizoglu1@gmail.com>
2024-03-20 04:37:45 +0300

    st-update

--- Streamlit/function.py
+++ Streamlit/function.py
@@ -176,9 +176,4 @@ def pred_data(df, df_input):
 
 
 
-@st.cache_data
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
